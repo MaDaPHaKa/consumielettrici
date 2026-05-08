db341ef3c6d6944a61f86b890ef3de6f27cb721f
lmolino <luca.molino@enginium.eu>
2026-05-08 10:50:12 +0200

    toggle modalità consumo

--- src/app/_components/edit-lettura/edit-lettura.component.ts
+++ src/app/_components/edit-lettura/edit-lettura.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
@@ -11,6 +12,8 @@ import { Lettura } from '@db/db';
 import { roundTo } from '@functions/utils';
 import { CambioAnnoService } from '@services/cambio-anno.service';
 
+type EditMode = 'lettura' | 'consumo';
+
 @Component({
   selector: 'app-edit-lettura',
   templateUrl: './edit-lettura.component.html',
@@ -18,6 +21,7 @@ import { CambioAnnoService } from '@services/cambio-anno.service';
   imports: [
     ReactiveFormsModule,
     MatButtonModule,
+    MatButtonToggleModule,
     MatCheckboxModule,
     MatDatepickerModule,
     MatFormFieldModule,
@@ -30,6 +34,7 @@ export class EditLetturaComponent extends AggiungiLetturaComponent {
   private readonly cambioAnnoService = inject(CambioAnnoService);
   letturaPrevG = 0;
   giornoCambioBaseline = false;
+  mode: EditMode = 'consumo';
 
   override ngOnInit(): void {
     this.lettura = this.data;
@@ -40,6 +45,7 @@ export class EditLetturaComponent extends AggiungiLetturaComponent {
       escludiDaMedia: new FormControl(this.lettura?.escludiDaMedia),
       escludiDaMinMax: new FormControl(this.lettura?.escludiDaMinMax),
     });
+    this.applyMode();
     const giornoPrima = this.utils.getGiornoPrima(this.lettura!.giorno);
     this.cambioAnnoService.getForGiorno(this.lettura!.giorno).subscribe({
       next: (currCamb) => {
@@ -52,10 +58,32 @@ export class EditLetturaComponent extends AggiungiLetturaComponent {
     });
   }
 
+  onModeChange(mode: EditMode) {
+    this.mode = mode;
+    this.applyMode();
+  }
+
+  private applyMode() {
+    const letturaCtrl = this.form.get('lettura');
+    const consumoCtrl = this.form.get('consumo');
+    if (this.mode === 'lettura') {
+      letturaCtrl?.enable({ emitEvent: false });
+      consumoCtrl?.disable({ emitEvent: false });
+    } else {
+      consumoCtrl?.enable({ emitEvent: false });
+      letturaCtrl?.disable({ emitEvent: false });
+    }
+  }
+
   override salva() {
     const lettura = this.form.get('lettura')?.value;
     const consumo = this.form.get('consumo')?.value;
-    if (lettura === 0 && consumo !== 0) {
+    if (this.mode === 'consumo') {
+      const letturaVal = this.giornoCambioBaseline
+        ? consumo
+        : consumo + this.letturaPrevG;
+      this.form.get('lettura')?.setValue(roundTo(letturaVal, 3));
+    } else if (lettura === 0 && consumo !== 0) {
       const letturaVal = this.giornoCambioBaseline
         ? consumo
         : consumo + this.letturaPrevG;
